158f459bc93e93c6734e95e4c3b17333d10f54fc
Andreas Bühl <code@abuehl.de>
2020-12-22 00:00:29 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="micropython-ahtx0",
+    version="0.1.0",
+    author="Andreas Bühl",
+    author_email="code@abuehl.de",
+    description="MicroPython driver for the AHT10 and AHT20 temperature and humindity sensors.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="aht10, aht20, micropython, temperature, humidity, sensor, i2c",
+    url="https://github.com/targetblank/micropython_ahtx0",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: Implementation :: MicroPython",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
